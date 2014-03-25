689886cf3cce705c0bf91872977ae49b3cefa180
Krystian Wieczorek <krystianwieczorek@o2.pl>
2014-03-25 07:06:44 +0100

    Implementacja RabatDAO

---
+++ s&kPBD/DAOHibernate/src/pl/edu/swsim/simon_wieczorek/RabatDAO.java
@@ -0,0 +1,13 @@
+package pl.edu.swsim.simon_wieczorek;
+
+import java.util.List;
+
+public interface RabatDAO {
+
+	public void add(Rabat rabat);
+	public void update(Rabat rabat);
+	public List<Rabat> get();
+	public List<Rabat> listId(int rabat_id);
+	public List<Rabat> listWysokosc(double wysokosc);
+	public void delete(int rabat_id);
+}

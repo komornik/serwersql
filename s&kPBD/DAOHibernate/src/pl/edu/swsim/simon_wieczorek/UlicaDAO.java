6cdad5e5e2b3f89fc33d8f9c802c29985f0724c3
Krystian Wieczorek <krystianwieczorek@o2.pl>
2014-05-14 08:44:48 +0200

    Uzupełnienie interfejsu DAO dla encji Ulica

--- s&kPBD/DAOHibernate/src/pl/edu/swsim/simon_wieczorek/UlicaDAO.java
+++ s&kPBD/DAOHibernate/src/pl/edu/swsim/simon_wieczorek/UlicaDAO.java
@@ -1,5 +1,13 @@
 package pl.edu.swsim.simon_wieczorek;
 
-public interface UlicaDAO {
+import java.util.List;
 
+public interface UlicaDAO {	
+	public void add(Ulica ulica);
+	public void delete(int ulica_id);
+	public Ulica get(int ulica_id);
+	public Ulica getUlica(String ulica);
+	public List<Ulica> get(String ulica);
+	public void update(Ulica ulica);
+	public List<Ulica> get();
 }

f7a16d9ededfe8d0b3bffa4f5ab5ac7fbac78a89
Krystian Wieczorek <krystianwieczorek@o2.pl>
2014-05-17 21:40:04 +0200

    Dodanie encji stanMagazynowy

---
+++ s&kPBD/DAOHibernate/src/pl/edu/swsim/simon_wieczorek/stanMagazynowy.java
@@ -0,0 +1,47 @@
+package pl.edu.swsim.simon_wieczorek;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.*;
+
+@Entity
+@Table(name="stan_magazynowy")
+public class stanMagazynowy{
+	
+	public int getStan_magazynowy_id() {
+		return stan_magazynowy_id;
+	}
+
+	public void setStan_magazynowy_id(int stan_magazynowy_id) {
+		this.stan_magazynowy_id = stan_magazynowy_id;
+	}
+
+	public Produkt getProdukt() {
+		return produkt;
+	}
+
+	public void setProdukt(Produkt produkt) {
+		this.produkt = produkt;
+	}
+
+	public Magazyn getMagazyn() {
+		return magazyn;
+	}
+
+	public void setMagazyn(Magazyn magazyn) {
+		this.magazyn = magazyn;
+	}
+
+	private int stan_magazynowy_id;
+	@ManyToOne
+	@JoinColumn(name="produkt_id")
+	private Produkt produkt;
+	
+	@ManyToOne
+	@JoinColumn(name="magazyn_id")
+	private Magazyn magazyn;
+	
+}
